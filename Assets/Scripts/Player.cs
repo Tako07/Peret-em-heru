9da74fa2da4a93192414410e025ec8d91ac44900
rubenlam <rubenlam_do@hotmail.com>
2022-08-14 15:51:39 -0500

    Cambio de paquete a InputSystem
    Por recomendaciones del taller, se usará el paquete InputSystem. Se tuvieron que hacer cambios en el movimiento del personaje y la cámara no sirve.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] float movmentSpeed = 10;
+    private Rigidbody2D rigidBody;
+    private Vector2 direction;
     void Start()
     {
-        
+        rigidBody = GetComponentInChildren<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movementVertical = Input.GetAxis("Vertical") * movmentSpeed * Time.deltaTime;
-        float movementHorizontal = Input.GetAxis("Horizontal") * movmentSpeed * Time.deltaTime;
-        transform.Translate(movementHorizontal, movementVertical, 0);
+    }
+    private void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + direction * movmentSpeed * Time.fixedDeltaTime);
+    }
+    void OnMove(InputValue value)
+    {
+        direction = value.Get<Vector2>();
     }
 }
